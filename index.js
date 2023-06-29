4a1d74ede2478fae1dd8eb2ca38948feaeb2b92b
SgtFreddy713 <captesxel@gmail.com>
2023-06-29 17:14:47 -0300

    tab button added
    Tab button that saves the url of the current active tab.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 console.log(leadsFromLocalStorage);
@@ -31,7 +32,6 @@ inputBtn.addEventListener("click", function() {
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
     inputEl.value = "";
     render(myLeads);
-    console.log(localStorage.getItem("myLeads"))
 });
 
 deleteBtn.addEventListener("dblclick", function() {
@@ -39,3 +39,11 @@ deleteBtn.addEventListener("dblclick", function() {
     myLeads = [];
     render(myLeads);
 });
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
